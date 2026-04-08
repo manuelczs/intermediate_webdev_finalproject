2609d175ff058ab8a4702457f64fb427f96f4fe1
manuel <manuelczs@outlook.com>
2026-04-07 22:14:12 -0400

    last commit

--- src/script.js
+++ src/script.js
@@ -1,13 +1,24 @@
+const calculateSimpleInterest = (principal, rate, time) => {
+    return (principal * rate * time) / 100;
+  }
+
+const calculateTotalPayableAmount = (principal, interestAmount) => {
+    return principal + interestAmount;
+}
+
 const calculate = () => {
   let p = document.getElementById("principal").value;
   let r = document.getElementById("rate").value;
   let t = document.getElementById("time").value;
-  let simpleInterest = (p * r * t) / 100;
-  let amount = p - simpleInterest;
 
   let result = document.getElementById("result");
+
+  let simpleInterest = calculateSimpleInterest(p, r, t);
+  let amount = calculateTotalPayableAmount(p + simpleInterest);
 
   result.innerHTML = `<div>Principal Amount: <span>${parseInt(p).toFixed(2)}</span></div>
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
   <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
 };
+
+export { calculateSimpleInterest, calculateTotalPayableAmount };
